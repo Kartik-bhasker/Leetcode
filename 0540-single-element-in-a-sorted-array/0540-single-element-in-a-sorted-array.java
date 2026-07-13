e1478dd21df7211f014593c286a18df320d40cbc
Kartik Bhasker <kartikbhasker1225@gmail.com>
2026-07-13 16:04:02 +0530

    Time: 0 ms (100%), Space: 52.8 MB (57.97%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,21 +3,24 @@ class Solution {
         if(nums.length==1){
             return nums[0];
         }
-        for(int i=0;i<nums.length;i++){
-            if(i==0){
-                if(nums[0]!=nums[1]){
-                    return nums[0];
-                }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[nums.length-1]!=nums[nums.length-2]){
+            return nums[nums.length-1];
+        }
+        int start=1;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
             }
-            else if(i==(nums.length-1)){
-                if(nums[nums.length-1]!=nums[nums.length-2]){
-                    return nums[nums.length-1];
-                }
+            if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2==1 && nums[mid]==nums[mid-1]){
+                start=mid+1;
             }
             else{
-                if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
-                    return nums[i];
-                }
+                end=mid-1;
             }
         }return -1;
     }
