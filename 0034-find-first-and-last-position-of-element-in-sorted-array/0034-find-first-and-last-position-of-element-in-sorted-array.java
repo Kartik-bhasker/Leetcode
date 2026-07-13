54c2684b9c2973a2400205be151d5093c96643fe
Kartik Bhasker <kartikbhasker1225@gmail.com>
2026-07-13 13:51:31 +0530

    Time: 0 ms (100%), Space: 48.4 MB (14.06%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,43 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1, -1};
-
-        int start = search(nums, target, true);
-        int end = search(nums, target, false);
-
-        ans[0] = start;
-        ans[1] = end;
-
-        return ans;
-    }
-
-    int search(int[] nums, int target, boolean isFirstOcc) {
-        int start = 0;
-        int end = nums.length - 1;
-        int ans = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (target > nums[mid]) {
-                start = mid + 1;
-            } 
-            else if (target < nums[mid]) {
-                end = mid - 1;
-            } 
-            else {
-                ans = mid;
-
-                if (isFirstOcc) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
+        int lb=lowerBound(nums,target);
+        int ub=upperBound(nums,target);
+        if(lb==nums.length || nums[lb]!=target){
+            return new int[]{-1,-1};
         }
-
-        return ans;
+        else{
+            return new int []{lb,ub-1};
+        }
     }
+    int lowerBound(int[] nums,int target){
+        int start=0;
+        int end=nums.length-1;
+        int ans=nums.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }return ans;
+    }
+    int upperBound(int[] nums,int target){
+        int start=0;
+        int end=nums.length-1;
+        int ans=nums.length;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>target){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }return ans;
+    }
+    
 }
