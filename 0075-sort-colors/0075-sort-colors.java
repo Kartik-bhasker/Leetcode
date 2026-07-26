7f3e954457e22c421c1d6fc5dcec1bf5028e1026
Kartik Bhasker <kartikbhasker1225@gmail.com>
2026-07-26 19:39:39 +0530

    Time: 0 ms (100%), Space: 43.7 MB (25.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,25 @@
 class Solution {
     public void sortColors(int[] nums){
-        int count0=0;
-        int count1=0;
-        int count2=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                count0++;
+        //DUTCH NATIONAL FLAG ALGO
+        int low=0;int mid=0;int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                //swap low and mid
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;mid++;    
             }
-            else if(nums[i]==1){
-                count1++;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                count2++;
+                //swap high and mid 
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
             }
-        }
-        for(int i=0;i<count0;i++){
-            nums[i]=0;
-        }
-        for(int i=count0;i<count0+count1;i++){
-            nums[i]=1;
-        }
-        for(int i=count0+count1;i<nums.length;i++){
-            nums[i]=2;
         }
     }
 }
