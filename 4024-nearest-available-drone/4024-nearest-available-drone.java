c26ceab946fefba856f72d9486b4ed254a16c9e6
Kartik Bhasker <kartikbhasker1225@gmail.com>
2026-08-16 21:40:44 +0530

    Time: 1 ms (100%), Space: 46.7 MB (19.41%) - LeetHub

---
+++ 4024-nearest-available-drone/4024-nearest-available-drone.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int nearestDrone(int[][] drones, int[] target) {
+        int ans=-1;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<drones.length;i++){
+            int dist=Math.abs(drones[i][0]-target[0])+Math.abs(drones[i][1]-target[1]);
+            if(dist<=drones[i][2]){
+                if(dist<min){
+                    min=dist;
+                    ans=i;
+                }
+            }
+        }return ans;
+    }
+}
